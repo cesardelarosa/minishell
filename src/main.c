6b444c3a9676c609a7a7d661c0f85a1da98c58ab
Adrian <adrian.pedrero.miro@gmail.com>
2024-09-19 17:04:10 +0200

    prueba

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: adrian <adrian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 17:48:49 by cde-la-r          #+#    #+#             */
-/*   Updated: 2024/09/19 16:14:36 by adrian           ###   ########.fr       */
+/*   Updated: 2024/09/19 17:03:34 by adrian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 int	main(void)
 {
 	printf(WELCOME_MSG);
+	printf("manuel");
 	setup_signal_handlers();
 	while (42)
 		handle_command(parse_command(read_input()));
