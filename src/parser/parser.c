37ca8d42a06482a7c49988382004989bff6932b1
cde-la-r <code@cesardelarosa.xyz>
2025-04-04 00:51:45 +0200

    new file structure

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: cde-la-r <code@cesardelarosa.xyz>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 18:08:20 by cde-la-r          #+#    #+#             */
-/*   Updated: 2025/04/04 00:34:37 by cesi             ###   ########.fr       */
+/*   Updated: 2025/04/04 00:50:04 by cesi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,57 +16,7 @@
 #include <stdlib.h>
 #include "lexer.h"
 
-int	process_token(t_command *cmd, t_list **tokens_ptr, t_list **arg_lst);
-
-static char	**build_argv_from_list(t_list *lst)
-{
-	size_t	count;
-	size_t	i;
-	char	**argv;
-	t_list	*current;
-
-	count = ft_lstsize(lst);
-	argv = ft_calloc(count + 1, sizeof(char *));
-	if (!argv)
-		return (NULL);
-	i = 0;
-	current = lst;
-	while (current)
-	{
-		argv[i++] = ft_strdup((char *)current->content);
-		current = current->next;
-	}
-	argv[i] = NULL;
-	return (argv);
-}
-
-static t_command	*build_command(t_list **tokens_ptr, int status)
-{
-	t_command	*cmd;
-	t_list		*arg_list;
-	t_token		*token;
-
-	(void)status;
-	cmd = ft_calloc(1, sizeof(t_command));
-	if (!cmd)
-		return (NULL);
-	arg_list = NULL;
-	while (*tokens_ptr)
-	{
-		token = (t_token *)(*tokens_ptr)->content;
-		if (token->type == TOKEN_PIPE || token->type == TOKEN_EOF)
-			break ;
-		if (!process_token(cmd, tokens_ptr, &arg_list))
-		{
-			free(cmd);
-			ft_lstclear(&arg_list, free);
-			return (NULL);
-		}
-	}
-	cmd->argv = build_argv_from_list(arg_list);
-	ft_lstclear(&arg_list, free);
-	return (cmd);
-}
+t_command	*build_command(t_list **tokens_ptr, int status);
 
 t_pipeline	*parser(t_list *tokens, t_ctx *ctx)
 {
