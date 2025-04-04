75cacbdba724b20db91d428a3d75028472c88e42
cde-la-r <code@cesardelarosa.xyz>
2025-04-04 10:41:20 +0200

    env builtins

--- src/exec/builtins/ft_env.c
+++ src/exec/builtins/ft_env.c
@@ -5,14 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cde-la-r <code@cesardelarosa.xyz>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/31 21:11:21 by cde-la-r          #+#    #+#             */
-/*   Updated: 2025/04/01 10:46:53 by cesi             ###   ########.fr       */
+/*   Created: 2025/04/04 10:17:25 by cde-la-r          #+#    #+#             */
+/*   Updated: 2025/04/04 10:17:27 by cde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "env.h"
+#include "libft.h"
+#include <stdio.h>
+
+static void	print_env_in_order(t_envvar *curr)
+{
+	if (!curr)
+		return ;
+	print_env_in_order(curr->next);
+	printf("%s=%s\n", curr->key, curr->value);
+}
+
 int	ft_env(char **argv, char **envp)
 {
-	(void)envp;
+	t_env	*env;
+
 	(void)argv;
+	env = env_init(envp);
+	if (!env)
+		return (1);
+	print_env_in_order(env->head);
+	env_destroy(env);
 	return (0);
 }
--- src/exec/builtins/ft_unset.c
+++ src/exec/builtins/ft_unset.c
@@ -5,14 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cde-la-r <code@cesardelarosa.xyz>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/31 21:11:21 by cde-la-r          #+#    #+#             */
-/*   Updated: 2025/04/01 10:47:12 by cesi             ###   ########.fr       */
+/*   Created: 2025/04/04 10:28:19 by cde-la-r          #+#    #+#             */
+/*   Updated: 2025/04/04 10:28:54 by cde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "env.h"
+#include "libft.h"
+#include <stdio.h>
+
 int	ft_unset(char **argv, char **envp)
 {
-	(void)envp;
-	(void)argv;
-	return (0);
+	t_env	*env;
+	int		i;
+	int		status;
+
+	env = env_init(envp);
+	if (!env)
+		return (1);
+	status = 0;
+	i = 1;
+	while (argv[i])
+	{
+		if (env_unset(env, argv[i]) != 0)
+			status = 1;
+		i++;
+	}
+	env_destroy(env);
+	return (status);
 }
