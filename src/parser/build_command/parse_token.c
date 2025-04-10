c66b5fded68111fbcb689f0805a85adfbf564f27
César de la Rosa <code@cesardelarosa.xyz>
2025-04-10 12:49:39 +0200

    small things

--- src/parser/build_command/parse_token.c
+++ src/parser/build_command/parse_token.c
@@ -24,14 +24,14 @@ int	parse_token(t_command *cmd, t_list **tokens_ptr, t_list **arg_lst,
 	int		result;
 
 	token = (t_token *)(*tokens_ptr)->content;
+	if (token->type == TOKEN_ERROR)
+		return (parse_error(token));
 	if (token->type == TOKEN_REDIRECT_IN
 		|| token->type == TOKEN_REDIRECT_OUT
 		|| token->type == TOKEN_HEREDOC
 		|| token->type == TOKEN_APPEND)
 		return (parse_redirection(cmd, tokens_ptr, token->type, ctx));
-	else if (token->type == TOKEN_ERROR)
-		return (parse_error(token));
-	else if (token->type == TOKEN_WORD
+	if (token->type == TOKEN_WORD
 		|| token->type == TOKEN_SINGLE_QUOTED_STRING
 		|| token->type == TOKEN_DOUBLE_QUOTED_STRING)
 	{
