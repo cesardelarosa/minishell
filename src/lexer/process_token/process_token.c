88e3c94ab3ee4afe025bfff23e60a1ed0ee18df2
cde-la-r <code@cesardelarosa.xyz>
2025-04-12 11:25:07 +0200

    process token update

--- src/lexer/process_token/process_token.c
+++ src/lexer/process_token/process_token.c
@@ -1,17 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lexer.c                                            :+:      :+:    :+:   */
+/*   process_token.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cde-la-r <code@cesardelarosa.xyz>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 17:20:26 by cde-la-r          #+#    #+#             */
-/*   Updated: 2025/04/04 17:22:36 by cesi             ###   ########.fr       */
+/*   Updated: 2025/04/12 11:22:06 by cesi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 #include "lexer_utils.h"
+#include "lexer_utils_bonus.h"
 #include "libft.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -30,11 +31,19 @@ int	process_token(char **s, t_list **tokens, int joined)
 			return (-1);
 		return (1);
 	}
+	if (**s == '&' && (*s)[1] == '&' && process_and(s, tokens) == 0)
+		return (1);
+	if (**s == '|' && (*s)[1] == '|' && process_or(s, tokens) == 0)
+		return (1);
 	if (**s == '|' && process_pipe(s, tokens) == 0)
 		return (1);
 	if (**s == '<' && process_redirect_in(s, tokens) == 0)
 		return (1);
 	if (**s == '>' && process_redirect_out(s, tokens) == 0)
+		return (1);
+	if (**s == ')' && process_rparen(s, tokens) == 0)
+		return (1);
+	if (**s == '(' && process_lparen(s, tokens) == 0)
 		return (1);
 	process_word(s, tokens, joined);
 	return (1);
