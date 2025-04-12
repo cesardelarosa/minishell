32117eb97c244acb90301f6a79aa4ff8c5901898
cde-la-r <code@cesardelarosa.xyz>
2025-04-12 13:23:10 +0200

    exec and main

--- src/main_bonus.c
+++ src/main_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cde-la-r <code@cesardelarosa.xyz>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 17:52:04 by cde-la-r          #+#    #+#             */
-/*   Updated: 2025/04/11 17:29:39 by cesi             ###   ########.fr       */
+/*   Updated: 2025/04/12 13:19:32 by cesi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,10 @@
 #include <readline/readline.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "ast_utils_bonus.h"
+
+int		ast_exec(t_ast *ast);
+t_ast	*ast_parser(t_list *tokens, t_ctx *ctx);
 
 extern volatile sig_atomic_t	g_sigint_received;
 
@@ -27,7 +31,7 @@ int	main(int argc, char **argv, char **envp)
 	t_ctx		ctx;
 	char		*line;
 	t_list		*tokens;
-	t_pipeline	*pipeline;
+	t_ast		*ast_root;
 
 	(void)argc;
 	(void)argv;
@@ -41,11 +45,13 @@ int	main(int argc, char **argv, char **envp)
 		tokens = lexer(line);
 		if (!tokens)
 			continue ;
-		pipeline = parser(tokens, &ctx);
-		if (!pipeline)
+		ast_root = ast_parser(tokens, &ctx);
+		ft_lstclear(&tokens, free_token);
+		if (!ast_root)
 			continue ;
 		setup_signals(COMMAND_MODE);
-		ctx.status = exec(pipeline);
+		ctx.status = ast_exec(ast_root);
+		ast_destroy(ast_root);
 	}
 	return (destroy_ctx(&ctx));
 }
