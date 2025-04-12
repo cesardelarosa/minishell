e3d9097b6e5a8c3bcdd25faf72b62f20f2757907
cde-la-r <code@cesardelarosa.xyz>
2025-04-12 11:48:38 +0200

    process word new

--- src/lexer/process_token/process_word.c
+++ src/lexer/process_token/process_word.c
@@ -6,7 +6,7 @@
 /*   By: cde-la-r <code@cesardelarosa.xyz>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 17:20:26 by cde-la-r          #+#    #+#             */
-/*   Updated: 2025/04/04 16:40:46 by cesi             ###   ########.fr       */
+/*   Updated: 2025/04/08 12:14:57 by cde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int	is_metachar(char c)
+{
+	if (c == '|' || c == '<' || c == '>' || c == '\'' || c == '\"' || c == '('
+		|| c == ')')
+		return (1);
+	return (0);
+}
+
 static char	*read_word(const char **s)
 {
 	const char	*start;
 	size_t		len;
 
 	start = *s;
-	while (**s && !ft_iswhitespace(**s) && **s != '|' && **s != '<'
-		&& **s != '>' && **s != '\'' && **s != '\"')
+	while (**s && !ft_iswhitespace(**s) && !is_metachar(**s)
+		&& !((*s)[0] == '&' && (*s)[1] == '&'))
 		(*s)++;
 	len = *s - start;
 	return (ft_substr(start, 0, len));
